76624d6bb2d48123c390541190d1c859948976ac
ElifBahar <elif.har94@gmail.com>
2023-08-19 02:06:38 +0300

    added a route directly to web php which returns a html code

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/returnHTML', function () {
+    return '<h1>Welcome</h1>';
+});
+
