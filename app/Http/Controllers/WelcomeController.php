0c8d1d44a4460b64d9d8ef3ed0cfcb28dd4d3ee9
ElifBahar <elif.har94@gmail.com>
2023-08-19 14:44:53 +0300

    added raw sql query and query builder fetch

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
     public function index(){
+        // 1. Using raw SQL queries //returns array
+        // $users = DB::select('select * from users');
+        // dd($users);
+
+        // 2. Query Builder //returns collection
+        // $users = DB::table('users')->select('name','email')->whereNotNull('email')->orderBy('name')->get();
+        // dd($users);
+
+        // 3. Eloquent ORM
+
+
         return view('welcome');
     }
 }
